f7aa5d293d6b51403b9d730cfbd5c8239b525302
WillJun_Cho <420658171@qq.com>
2018-01-17 14:35:56 +0800

    添加我的页面文件

---
+++ Runner/Runner/Main/个人/View/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  Runner
+//
+//  Created by ☁️WillJun_Cho on 17/01/2018.
+//  Copyright © 2018 ☁️WillJun_Cho. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
